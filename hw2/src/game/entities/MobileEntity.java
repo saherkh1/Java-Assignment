9326bc7bcc79fde5605184a3ef618a1dd0029ea3
seenaabass <seenaabass34@gmail.com>
2019-04-07 02:20:50 +0300

    seena 7.4.19 2:19 MobileEntity & Sprotsman change

--- hw2/src/game/entities/MobileEntity.java
+++ hw2/src/game/entities/MobileEntity.java
@@ -1,5 +1,38 @@
 package game.entities;
 
-public class MobileEntity {
+public class MobileEntity extends Entity{
+	private double maxSpeed;
+	private double acceleration;
+	private double Speed;
+	
+	public MobileEntity(double maxSpeed, double acceleration){
+		super();
+		this.setMaxSpeed(maxSpeed);
+		this.setAcceleration(acceleration);
+		this.setSpeed(0);
+	}
 
+	public double getMaxSpeed() {
+		return maxSpeed;
+	}
+
+	public void setMaxSpeed(double maxSpeed) {
+		this.maxSpeed = maxSpeed;
+	}
+
+	public double getAcceleration() {
+		return acceleration;
+	}
+
+	public void setAcceleration(double acceleration) {
+		this.acceleration = acceleration;
+	}
+
+	public double getSpeed() {
+		return Speed;
+	}
+
+	public void setSpeed(double speed) {
+		Speed = speed;
+	}
 }
--- hw2/src/game/entities/sportsman/Sportsman.java
+++ hw2/src/game/entities/sportsman/Sportsman.java
@@ -1,5 +1,41 @@
 package game.entities.sportsman;
 
-public class Sportsman {
+import game.entities.MobileEntity;
 
+public class Sportsman extends MobileEntity{
+	
+	private String name;
+	private double age;
+	private String gender;
+	
+	public Sportsman(String name, double age, String gender, double maxSpeed, double acceleration) {
+		super(maxSpeed, acceleration);
+		this.setName(name);
+		this.setGender(gender);
+		this.setAge(age);
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public double getAge() {
+		return age;
+	}
+
+	public void setAge(double age) {
+		this.age = age;
+	}
 }
